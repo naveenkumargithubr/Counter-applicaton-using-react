5167fd08176988fd9667194c89327066bddc17a9
naveenkumargithubr <112088541+naveenkumargithubr@users.noreply.github.com>
2022-09-18 10:56:20 +0530

    Updating the Counter Application

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,18 +1,28 @@
 import {Component} from 'react'
 import './index.css'
 class Counter extends Component {
+  state = {count :0}  //.................................. intialaising the state object
   onIncrement = () => {
-    console.log('increment clicked')
+  //.......................................................now Update the state Object
+    this.setState(prevState => {
+      console.log(`previous State value is ${prevState.count}`)
+   // now return the changed value
+      return {count : prevState.count + 1}
   }
+                  
   onDecrement = () => {
-    console.log('Decrement cliked')
+    this.setState(prevState => {
+      console.log(`previous State value is ${prevState.count}`)
+   // now return the changed value
+      return {count : prevState.count - 1}
   }
 
   render() {
+    const {count} = this.state //........................object destructuring the state object
     return (
       <div className="container">
         <h1 className="heading">Counter</h1>
-        <p className="count">0</p>
+        <p className="count">{state}</p>
         <div>
           <button className="button" onClick={this.onIncrement}>
             Increase
